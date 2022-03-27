46cc1611c4de268ad525dd1bcf2e3e8a97958431
Alexey Alexeev <siroezhka29@yandex.ru>
2022-03-27 18:41:41 +0400

    secondRefactor

--- src/main/java/com/Scheduled/Scheduled_server/parser/EpicGamesClient.java
+++ src/main/java/com/Scheduled/Scheduled_server/parser/EpicGamesClient.java
@@ -1,21 +1,25 @@
 package com.Scheduled.Scheduled_server.parser;
 
 import com.Scheduled.Scheduled_server.model.Game;
-import com.Scheduled.Scheduled_server.utils.EpicGamesClientHelper;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
+import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static com.Scheduled.Scheduled_server.utils.Constants.ATTRIBUTE_HREF;
+import static com.Scheduled.Scheduled_server.utils.Constants.NO_VALID_PRICE_GAME;
 import static com.Scheduled.Scheduled_server.utils.Constants.PAGINATION_START;
 import static com.Scheduled.Scheduled_server.utils.Constants.PAGINATION_STEP;
+import static com.Scheduled.Scheduled_server.utils.Constants.REGEX_PATTERN_RUSSIAN_VERSION;
 import static com.Scheduled.Scheduled_server.utils.Constants.SELECTOR_FOOTER_LI;
 import static com.Scheduled.Scheduled_server.utils.Constants.SELECTOR_GAMES;
 import static com.Scheduled.Scheduled_server.utils.Constants.START_URL;
@@ -24,36 +28,50 @@ import static com.Scheduled.Scheduled_server.utils.Constants.START_URL;
 @RequiredArgsConstructor
 public class EpicGamesClient {
     private final GameParser gameParser;
+
     public List<Game> loadGames() throws IOException {
-        List<Game> games = new Vector<>();
-        IntStream
-                .range(PAGINATION_START,
-                        EpicGamesClientHelper
-                                .pagesCount(
-                                        Jsoup
-                                                .connect(START_URL + PAGINATION_START).get()
-                                                .select(SELECTOR_FOOTER_LI)
-                                                .stream()
-                                                .map(Element::text).collect(Collectors.toList()))
-                )
-                .map(current -> current * PAGINATION_STEP)
-                .boxed().map(current -> START_URL + current)
+        return IntStream
+                .range(PAGINATION_START, pagesCount())
+                .mapToObj(current -> START_URL + current * PAGINATION_STEP)
                 .collect(Collectors.toList())
-                .parallelStream().forEach(url -> {
+                .parallelStream()
+                .flatMap(url -> {
+                            try {
+                                return Jsoup.connect(url).get()
+                                        .select(SELECTOR_GAMES).parallelStream();
 
-            try {
-                games.addAll(Jsoup.connect(url).get()
-                        .select(SELECTOR_GAMES).stream()
-                        .filter(EpicGamesClientHelper::isRussianVersion)
-                        .map(gameParser::parseGame)
-                        .filter(EpicGamesClientHelper::isValidGame)
-                        .distinct().collect(Collectors.toList()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                return Stream.empty();
+                            }
+                        }
+                )
+                .filter(this::isRussianVersion)
+                .map(gameParser::parseGame)
+                .filter(this::isValidGame).collect(Collectors.toList());
+    }
 
 
-        });
-        return new ArrayList<>(games);
+    private int pagesCount() throws IOException {
+        return
+                Jsoup
+                        .connect(START_URL + PAGINATION_START).get()
+                        .select(SELECTOR_FOOTER_LI)
+                        .stream()
+                        .map(Element::text)
+                        .filter(e -> !e.equals(Strings.EMPTY))
+                        .mapToInt(Integer::parseInt)
+                        .max().orElseThrow(NoSuchElementException::new);
+
+
+    }
+
+    private boolean isRussianVersion(Element elementId) {
+        return Pattern.compile(REGEX_PATTERN_RUSSIAN_VERSION)
+                .matcher(elementId.attr(ATTRIBUTE_HREF)).find();
+    }
+
+    private boolean isValidGame(Game game) {
+        return game.getGameBase().getPrice() != NO_VALID_PRICE_GAME;
     }
 }
